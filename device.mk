21b27a45fe9e34f7d26ff9bef6daabf43634762f
rahulsingh0012 <rahulrajsn35@gmail.com>
2024-10-21 19:54:03 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,6 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
